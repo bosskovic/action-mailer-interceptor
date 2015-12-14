e13b48a7cdd15d7ceb907bd569f6ccb10f092dd2
Marko Bošković <bosskovic@gmail.com>
2015-12-14 21:36:42 +0100

    fix

--- lib/actionmailer_interceptor/mail_interceptor.rb
+++ lib/actionmailer_interceptor/mail_interceptor.rb
@@ -1,21 +1,19 @@
-module ActionmailerInterceptor
-  class MailInterceptor
-    attr_accessor :redirection_email
+class ActionmailerInterceptor::MailInterceptor
+  attr_accessor :redirection_email
 
-    def initialize(options={})
-      @redirection_email = options[:redirection_email]
-    end
+  def initialize(options={})
+    @redirection_email = options[:redirection_email]
+  end
 
-    def self.delivering_email(message)
-      original_recipients = ' ['
-      [:to, :cc, :bcc].each do |field|
-        unless message.send(field).blank?
-          original_recipients << "#{field}: #{message.send(field).join(', ')}"
-        end
+  def self.delivering_email(message)
+    original_recipients = ' ['
+    [:to, :cc, :bcc].each do |field|
+      unless message.send(field).blank?
+        original_recipients << "#{field}: #{message.send(field).join(', ')}"
       end
-      original_recipients << ']'
-      message.subject += original_recipients
-      message.to = @redirection_email
     end
+    original_recipients << ']'
+    message.subject += original_recipients
+    message.to = @redirection_email
   end
 end
